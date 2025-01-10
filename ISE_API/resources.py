5ad72474d8cdeb0a343d7618167209ceaaa713b0
CiscoPrime <95670787+CiscoPrime@users.noreply.github.com>
2025-01-10 12:07:41 +0000

    Update resources.py

--- ISE_API/resources.py
+++ ISE_API/resources.py
@@ -15,5 +15,22 @@ SUPPORTED_RESOURCES = {
             "_QPH_": "Y29tbWFuZD1zeW5jdXBEQg==",  # Special header specific to `sync_action`
             "X-Requested-With": "XMLHttpRequest"  # Resource-specific header for sync
         }
+    },
+    "radius_policies": {
+        "path": "admin/rs/uiapi/policytable/radius",
+        "method": "GET",
+        "requires_csrf": False,
+        "params": [],  # No extra parameters for this request
+        "headers": {}  # No custom headers needed for this request
+    },
+    "system_summary": {
+        "path": "admin/rs/uiapi/dashboard/generic/fetchData",
+        "method": "GET",
+        "requires_csrf": True,
+        "params": [],  # No extra parameters for this request
+        "headers": {
+            "_QPH_": "cXVlcnk9c3lzdGVtU3VtbWFyeQ==",
+            "X-Requested-With": "XMLHttpRequest, XMLHttpRequest"
+        }  # No custom headers needed for this request
     }
 }
