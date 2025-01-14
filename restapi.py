57cb49d1125c104c99af5c4bf1c1d785e3e2df88
Shane <shane.langley3@homeoffice.gov.uk>
2025-01-14 10:36:28 +0000

    Replace entire repository contents with local workspace contents

--- restapi.py
+++ restapi.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/restart_ap', methods=['POST'])
 def restart_ap():
-    ap_name = request.json.get("ap_name")
+    ap_name = request.json.get("HO-HDC-AP012-02")
     with manager.connect(
-        host='WLC_IP',
+        host='10.23.20.140',
         port=830,
-        username='your_username',
-        password='your_password',
+        username='p-langles3',
+        password='Bo?VrPu9yz+IE*n4dEJh',
         hostkey_verify=False
     ) as m:
         rpc_command = f"""
